2b4b793cdd175dfbe9eebc5c6521221edb8b2881
Akshata <honrao.a@northeastern.edu>
2019-08-01 23:26:36 -0400

    cloud formations scripts added

---
+++ cloudformation/csye6225-aws-cf-create-lambda-stack.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#shell script to create AWS network infrastructures
+
+
+ help_me()
+{
+	  echo "Usage:-"
+	  echo "$0 <Application-Stack-Name> <S3CodeDeployBucket> <User> <fromaddress>"
+	  exit
+}
+
+	APP_STACK_NAME=$1-LambdaStack
+  CODEDEPLOYBUCKET=$2
+  USER=$3
+  FROMADDRESS=$4
+
+
+	if [ $# -ne 4 ]
+	then
+		echo -e "You are missing some parameters"
+		help_me
+fi
+
+echo $APP_STACK_NAME
+echo "Creating stack..."
+aws cloudformation create-stack --stack-name $APP_STACK_NAME --template-body file://csye6225-cf-lambda.json \
+--parameters ParameterKey=S3CodeDeployBucket,ParameterValue=$CODEDEPLOYBUCKET\
+ ParameterKey=CFNUser,ParameterValue=$USER\
+ ParameterKey=fromaddress,ParameterValue=$FROMADDRESS\
+ --capabilities CAPABILITY_NAMED_IAM
+
+if [ $? -eq 0 ]; then
+echo "Creating progress..."
+ aws cloudformation wait stack-create-complete --stack-name $APP_STACK_NAME
+ 	if [ $? -ne 255 ]; then
+
+  		echo "Stack created successfully!!"
+  	else
+  		echo "Failure while waiting for stack-create-complete !!"
+  	fi
+
+else
+  echo "Failure while creating stack !!"
+
+fi
